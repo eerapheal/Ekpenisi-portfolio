eee48f7229f46a7aa378e394c2073453a2801d6c
unknown <ekpenisiraphael@gmail.com>
2022-09-16 23:37:07 +0530

    local commit

--- script.js
+++ script.js
@@ -1,55 +1,134 @@
-// hamburger menu to click and onclick dropdown menu items.
+// const modalInformation = {
+//   heading1: 'Multi Post Stories',
+//   title2: 'Keeping track of hundreds  of components website',
+//   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+//   description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+//   imageSrc: './images/todolist.png',
+//   button1: 'See Live  <img src="./images/icon.png">',
+//   button2: 'See Source <img src="./images/vector.png">',
+//   li: 'html',
+//   li2: 'Bootstrap',
+//   li3: 'Ruby on rails',
+// };
 
-const hamburger = document.querySelector('.ambuger');
-const navItem = document.querySelector('.nav-item');
+// const div1 = document.createElement('div');
+// div1.className = 'modal';
+// document.body.appendChild(div1);
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navItem.classList.toggle('active');
-});
+// const div = document.createElement('div');
+// div.className = 'modal-header';
+// div1.appendChild(div);
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navItem.classList.remove('active');
-}));
+// const div2 = document.createElement('div');
+// div2.className = 'title';
+// div.appendChild(div2);
 
-// pop-up window in my see project section done with two javascript method.
+// const h2 = document.createElement('h1');
+// h2.className = 'modal-headm';
+// h2.textContent = modalInformation.heading1;
+// div2.appendChild(h2);
 
-function displayProjects() {
-  const div = document.getElementById('projectAera');
-  div.innerHTML = `
-  <section  class="second-sec">
-  <div id ="my-recnt">
-  <h3 class="second-tittle">
-    My Recent Works
-  </h3>
-  <hr class="line2">
-</div>
-<div id="multi">
-  <img class="img-desk" width="327" height="237" src="./images/Img-Placeholder.png" alt="">
-  <img class="img-desk2" width="327" height="237" src="./images/Img-Placeholder.png" alt="">
-  <div class="post">
-  <h3 class="second-subtittle">
-    Multi-Post Stories
-  </h3>
-  <p class="second-p">
-    A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
-  </p>
-  <div onclick="appear()">
-    <ul class="list-btn">
-      <li class="css-btn">css</li>
-      <li class="html-btn">html</li>
-      <li class="boot-btn">bootsrap</li>
-      <li class="ruby-btn">ruby</li>
-    </ul>
-    <button class="btn-top" onclick="appear()" type="button">See projests</button>
-  </div>
-</div>
-</div>
-</section>`;
-}
+// const h20 = document.createElement('div');
+// h20.className = 'modal-header22';
+// div2.appendChild(h20);
 
-displayProjects();
+// const h22 = document.createElement('h1');
+// h22.className = 'modal-headd';
+// h22.textContent = modalInformation.title2;
+// h20.appendChild(h22);
+
+// const span = document.createElement('button');
+// span.className = 'close-button';
+// span.innerHTML = '&times;';
+// div.appendChild(span);
+
+// const div3 = document.createElement('div');
+// div3.className = 'modal-tags';
+// div1.appendChild(div3);
+
+// const ul = document.createElement('ul');
+// ul.className = 'modal-social';
+// div3.appendChild(ul);
+
+// const li = document.createElement('li');
+// li.className = 'tags1';
+// li.innerHTML = modalInformation.li;
+// ul.appendChild(li);
+
+// const li2 = document.createElement('li');
+// li2.className = 'tags2';
+// li2.innerHTML = modalInformation.li2;
+// ul.appendChild(li2);
+
+// const li3 = document.createElement('li');
+// li3.className = 'tags3';
+// li3.innerHTML = modalInformation.li3;
+// ul.appendChild(li3);
+
+// const div4 = document.createElement('div');
+// div4.className = 'modal-pic';
+// div1.appendChild(div4);
+
+// const div12 = document.createElement('div');
+// div12.className = 'modal-body';
+// div4.appendChild(div12);
+
+// const p = document.createElement('p');
+// p.className = 'modal-mobile';
+// p.textContent = modalInformation.description2;
+// div12.appendChild(p);
+
+// const p2 = document.createElement('p');
+// p2.className = 'modal-desktop';
+// p2.textContent = modalInformation.description;
+// div12.appendChild(p2);
+
+// const div13 = document.createElement('div');
+// div13.className = 'modal-button';
+// div1.appendChild(div13);
+
+// const button = document.createElement('button');
+// button.className = 'live-button';
+// button.innerHTML = modalInformation.button1;
+// div13.appendChild(button);
+
+// const button2 = document.createElement('button');
+// button2.className = 'source-button';
+// button2.innerHTML = modalInformation.button2;
+// div13.appendChild(button2);
+
+// const parentModal = document.querySelector('.modal');
+// const openModalButtons = document.querySelector('.btn');
+// const closeModalButtons = document.querySelector('.close-button');
+// const overlay = document.querySelector('#main');
+// const openModalButtons2 = document.querySelector('.btn1');
+// const openModalButtons3 = document.querySelector('.btn2');
+// const openModalButtons4 = document.querySelector('.btn3');
+// const openModalButtons5 = document.querySelector('.btn4');
+// const openModalButtons6 = document.querySelector('.btn5');
+// const openModalButtons7 = document.querySelector('.btn6');
+
+// function appear() {
+//   parentModal.style.display = 'block';
+//   overlay.style.filter = 'blur(10px)';
+// }
+
+// openModalButtons.addEventListener('click', appear);
+
+// function disappear() {
+//   parentModal.style.display = 'none';
+//   overlay.style.filter = 'blur(0px)';
+// }
+
+// closeModalButtons.addEventListener('click', disappear);
+
+// function dissappearParent(e) {
+//   if (e.target.className === '.modal') {
+//     parentModal.style.display = 'none';
+//     overlay.style.filter = 'blur(0px)';
+//   }
+// }
+// parentModal.addEventListener('click', dissappearParent);
 
 const projects = [
   {
@@ -141,116 +220,39 @@ for (let index = 0; index < projects.length; index += 1) {
   htmlPage.appendChild(divProject);
 }
 
-const modalInformation = {
-  heading1: 'Multi Post Stories',
-  title2: 'Keeping track of hundreds  of components website',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-  description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  imageSrc: '<img src="./images/SnapshootPortfolio.png">',
-  button1: 'See Live  <img src="./images/Icon-Export.png">',
-  button2: 'See Source <img src="./images/Vectorgit.png">',
-  li: 'html',
-  li2: 'Bootstrap',
-  li3: 'Ruby on rails',
-};
 
-const div1 = document.createElement('div');
-div1.className = 'modal';
-document.body.appendChild(div1);
-
-const div = document.createElement('div');
-div.className = 'modal-header';
-div1.appendChild(div);
-
-const div2 = document.createElement('div');
-div2.className = 'title';
-div.appendChild(div2);
-
-const h2 = document.createElement('h1');
-h2.className = 'modal-headm';
-h2.textContent = modalInformation.heading1;
-div2.appendChild(h2);
-
-const h20 = document.createElement('div');
-h20.className = 'modal-header22';
-div2.appendChild(h20);
-
-const h22 = document.createElement('h1');
-h22.className = 'modal-headd';
-h22.textContent = modalInformation.title2;
-h20.appendChild(h22);
-
-const span = document.createElement('button');
-span.className = 'close-button';
-span.innerHTML = '&times;';
-div.appendChild(span);
-
-const div3 = document.createElement('div');
-div3.className = 'modal-tags';
-div1.appendChild(div3);
-
-const ul = document.createElement('ul');
-ul.className = 'modal-social';
-div3.appendChild(ul);
-
-const li = document.createElement('li');
-li.className = 'tags1';
-li.innerHTML = modalInformation.li;
-ul.appendChild(li);
-
-const li2 = document.createElement('li');
-li2.className = 'tags2';
-li2.innerHTML = modalInformation.li2;
-ul.appendChild(li2);
-
-const li3 = document.createElement('li');
-li3.className = 'tags3';
-li3.innerHTML = modalInformation.li3;
-ul.appendChild(li3);
-
-const div4 = document.createElement('div');
-div4.className = 'modal-pic';
-div1.appendChild(div4);
-
-const div12 = document.createElement('div');
-div12.className = 'modal-body';
-div4.appendChild(div12);
-
-const p = document.createElement('p');
-p.className = 'modal-mobile';
-p.textContent = modalInformation.description2;
-div12.appendChild(p);
-
-const p2 = document.createElement('p');
-p2.className = 'modal-desktop';
-p2.textContent = modalInformation.description;
-div12.appendChild(p2);
-
-const div13 = document.createElement('div');
-div13.className = 'modal-button';
-div1.appendChild(div13);
-
-const button = document.createElement('button');
-button.className = 'live-button';
-button.innerHTML = modalInformation.button1;
-div13.appendChild(button);
-
-const button2 = document.createElement('button');
-button2.className = 'source-button';
-button2.innerHTML = modalInformation.button2;
-div13.appendChild(button2);
-
-const parentModal = document.querySelector('.modal');
-// const openModalButtons = document.querySelector('multi-btn');
-const closeModalButtons = document.querySelector('.close-button');
-function appear() {
-  parentModal.style.display = 'block';
-}
-// openModalButtons.addEventListener('click', appear);
-appear();
-function disappear() {
-  parentModal.style.display = 'none';
+function displayProjects() {
+  const div = document.getElementById('projectAera');
+  div.innerHTML = `
+  <section  class="second-sec">
+  <div id ="my-recnt">
+  <h3 class="second-tittle">
+    My Recent Works
+  </h3>
+  <hr class="line2">
+</div>
+<div id="multi">
+  <img class="img-desk" width="327" height="237" src="./images/Img-Placeholder.png" alt="">
+  <img class="img-desk2" width="327" height="237" src="./images/Img-Placeholder.png" alt="">
+  <div class="post">
+  <h3 class="second-subtittle">
+    Multi-Post Stories
+  </h3>
+  <p class="second-p">
+    A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
+  </p>
+  <div onclick="appear()">
+    <ul class="list-btn">
+      <li class="css-btn">css</li>
+      <li class="html-btn">html</li>
+      <li class="boot-btn">bootsrap</li>
+      <li class="ruby-btn">ruby</li>
+    </ul>
+    <button class="btn-top" onclick="appear()" type="button">See projests</button>
+  </div>
+</div>
+</div>
+</section>`;
 }
 
-closeModalButtons.addEventListener('click', disappear);
-
+displayProjects();
